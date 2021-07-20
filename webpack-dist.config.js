724f27ba7dcfa34c7d4525da798d9c2d9eb6ef00
Devin Gong <devingong@cherrypicks.com>
2021-07-20 09:46:13 +0800

    sss

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -7,6 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const EndWebpackPlugin = require('end-webpack-plugin');
 const { WebPlugin } = require('web-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const puppeteer = require("puppeteer");
 
 const outputPath = path.resolve(__dirname, 'docs');
 module.exports = {
@@ -52,15 +53,5 @@ module.exports = {
       filename: '[name]_[contenthash].css',
     }),
     new OptimizeCSSAssetsPlugin(),
-    new EndWebpackPlugin(async () => {
-      // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
-
-      // 调用 Chrome 渲染出 PDF 文件
-      const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.wuhaolin.cn' // 这里注意改成你的在线简历的网站
-      ]);
-    }),
   ]
 };
